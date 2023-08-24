25a9243b0885c024db8fc1033d984fb03b537e09
SamenaBasith <115994259+SamenaBasith@users.noreply.github.com>
2023-08-24 22:43:00 +0100

    added a chosen word feature

--- Readme.md
+++ Readme.md
@@ -11,8 +11,10 @@ Python3
 
 To get started start:
 
-clone the repo to your local machine and made sure you cd into the right folder where you have cloned it
+1. Clone the repo to your local machine and made sure you cd into the right folder where you have cloned it.
 
-next, simply type in the following command in your terminal:
+2. Next, simply type in the following command in your terminal:
 
-python my_python_app.py
+   'python my_python_app.py'
+
+Answer the questions in the terminal- hint try your name as a chosen word to see if it incoorperates it!
--- my_python_app.py
+++ my_python_app.py
@@ -1,11 +1,12 @@
-from ASCII import welcomeKPMG
 import random
 import string
-
+from ASCII import welcomeKPMG
 
 def password_generator():
     print(welcomeKPMG)
-
+    
+    your_chosen_word = input("Enter a word to base your password on: ")
+    
     while True:
         try:
             password_length = int(input("Enter the password length you need (12 or more characters): "))
@@ -15,32 +16,34 @@ def password_generator():
                 print("Password length should be 12 or more characters.")
         except ValueError:
             print("Please enter a valid number.")
-    
 
-    num_uppercase = int(input("Enter the number of uppercase letters you want in your password (enter 4 or more thats what KPMG wants :) : "))
+    num_uppercase = int(input("Enter the number of uppercase letters you want in your password (4 or more, as KPMG suggests): "))
     
-
     include_lowercase = input("Include lowercase letters? (y/n): ").lower() == 'y'
     include_digits = input("Include digits? (y/n): ").lower() == 'y'
     include_special_characters = input("Include special characters? (y/n): ").lower() == 'y'
-    
-  
+
     if not (include_lowercase or include_digits or include_special_characters):
         print("At least one character must be selected to create the password.")
-        
         return
-    character_sets = ""
+    
+    characters = ""
     if include_lowercase:
-        character_sets += string.ascii_lowercase
+        characters += string.ascii_lowercase
     if include_digits:
-        character_sets += string.digits
+        characters += string.digits
     if include_special_characters:
-        character_sets += string.punctuation
+        characters += string.punctuation
 
+    your_chosen_word = your_chosen_word.replace(" ", "").lower()
+    max_available_characters = password_length - len(your_chosen_word) - num_uppercase
+    additional_characters_needed = random.choices(characters, k=max_available_characters)
+    
 
+    additional_characters_needed = additional_characters_needed[:max(0, password_length - len(your_chosen_word) - num_uppercase)]
+    
     uppercase_letters = random.sample(string.ascii_uppercase, num_uppercase)
-    password_characters = random.choices(character_sets, k=password_length - num_uppercase)
-    password_characters.extend(uppercase_letters)
+    password_characters = list(your_chosen_word) + additional_characters_needed + uppercase_letters
     random.shuffle(password_characters)
     password = ''.join(password_characters)
     
